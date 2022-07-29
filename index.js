83a405110192611d4f49a77b5cf87ad5fcc9b202
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-07-29 14:33:36 +0100

    complete project

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer ({dest: 'uploads/'})
 require('dotenv').config()
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// var handlebars = require('express-handlebars').create({ defaultLayout: null });
+// app.engine('handlebars', handlebars.engine);
+// app.set('view engine', 'handlebars');
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,6 +24,15 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  var response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+
+  res.json(response);
+})
 
 
 const port = process.env.PORT || 3000;
